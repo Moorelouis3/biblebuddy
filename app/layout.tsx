7e991217633b8e4166105a58a0dacaa3e79580b2
Louis Moore <moorelouis3@gmail.com>
2026-05-27 09:26:09 +0200

    Update app icon to final promise icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,9 +27,9 @@ export const metadata: Metadata = {
     canonical: siteUrl.toString(),
   },
   icons: {
-    icon: "/FinalIconREally.png",
-    apple: "/FinalIconREally.png",
-    shortcut: "/FinalIconREally.png",
+    icon: "/FinalIconIPromiseForAWhile.png",
+    apple: "/FinalIconIPromiseForAWhile.png",
+    shortcut: "/FinalIconIPromiseForAWhile.png",
   },
   manifest: "/manifest.json",
   appleWebApp: {
